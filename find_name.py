bd67d7e89fedd295093de183e0edd7a5d453af9d
Khue Dao <73230749+KhueDao29@users.noreply.github.com>
2023-12-25 16:19:17 +0700

    Update

--- find_name.py
+++ find_name.py
@@ -1,14 +1,15 @@
 import pandas as pd
+import time
+import json
 
 '''
 This program finds the meaning of the input name
 '''
+van_mau =  "Xin lỗi, chúng mình chưa có dữ liệu về tên của bạn. Bạn có thể hỗ trợ chúng mình bằng cách điền vào khảo sát bên dưới! ^-^"
 
 def find_meaning(filename, fullname):
     '''
-        This function return the meaning, name_single1, sn_vn1, rad1,
-                                pic1, name_single2, sn_vn2, rad2, pic2
-        of the input name.
+        This function return related data of the input name.
         
         Parameter:
             - filename str: directory of the csv data file
@@ -20,62 +21,33 @@ def find_meaning(filename, fullname):
     right = len(pf) - 1
     
     if len(name) > 1: #user input more than 1 words 
-        name = name[-2:]
-        name = ' '.join(name) #only assess the firstname
-    
-        i = search(pf, name, 'name-double') 
+        name = ' '.join(name[-2:]) #only assess the firstname
 
-        if i != -1:
-            meaning = pf.iloc[i]['meaning']
-            name_single1 = pf.iloc[i]['name-single1']
-            sn_vn1 = pf.iloc[i]['sinovietnamese1']
-            rad1 = pf.iloc[i]['radical1']
-            pic1 = pf.iloc[i]['image1']
-            name_single2 = pf.iloc[i]['name-single2']
-            sn_vn2 = pf.iloc[i]['sinovietnamese2']
-            rad2 = pf.iloc[i]['radical2']
-            pic2 = pf.iloc[i]['image2']
-        
-        else: #no data 
-            meaning = "Hiện tại chúng mình chưa có dữ liệu về tên của bạn :(" 
-            name_single1 = ""
-            sn_vn1 = ""
-            rad1 = ""
-            pic1 = ""
-            name_single2 = ""
-            sn_vn2 = ""
-            rad2 = ""
-            pic2 = ""
-        
+        result = pf[pf['name-double'] == name]
+        single = False
+
     else: #user only input 1 word 
         name = name[0] 
-        i = search(pf, name, 'name-single2')
+        result = pf[pf['name-single2'] == name]
+        single = True
 
-        if i != -1: 
-            meaning = pf.iloc[i]['radical2']
-            name_single1 = pf.iloc[i]['name-single2']
-            sn_vn1 = pf.iloc[i]['sinovietnamese2']
-            rad1 = pf.iloc[i]['radical2']
-            pic1 = pf.iloc[i]['image2']
-            name_single2 = ""
-            sn_vn2 = ""
-            rad2 = ""
-            pic2 = ""
-            
-            
-        else: #no data
-            meaning = "Hiện tại chúng mình chưa có dữ liệu về tên của bạn :(" 
-            name_single1 = ""
-            sn_vn1 = ""
-            rad1 = ""
-            pic1 = ""
-            name_single2 = ""
-            sn_vn2 = ""
-            rad2 = ""
-            pic2 = ""
-        
-
-    return meaning, name_single1, sn_vn1, rad1, pic1, name_single2, sn_vn2, rad2, pic2
+    if result.shape[0] == 0:
+        return json.dumps({"name-double": "",
+                           "meaning": van_mau,
+                           "name-single1": "", "sinovietnamese1": "", "radical1": "", "image1": "",
+                           "name-single2": "", "sinovietnamese2": "", "radical2": "", "image2": ""})
+    
+    elif single:
+        result = result.iloc[0].to_dict()
+        result['name-double'] = result['radical2']
+        result['name-single1'] = ''
+        result['sinovietnamese1'] = ''
+        result['image1'] = ''
+           
+        return json.dumps(result)
+    
+    else:
+        return result.iloc[0].to_json()
 
 def search(pf, name, column):
     '''
@@ -95,7 +67,6 @@ def main():
     testing 
     '''
     print(find_meaning('final.csv',"An"))
-
     
 if __name__ == '__main__':
         main()
